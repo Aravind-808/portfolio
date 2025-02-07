3907d72f222f42208b2abf181b9ed9145b02a85c
Aravind-808 <halfwind2005@gmail.com>
2025-02-07 13:35:37 +0530

    rounded borders and spacing :))

--- app/photogallery/page.tsx
+++ app/photogallery/page.tsx
@@ -2,40 +2,40 @@ export default function picsGallery(){
     return(
         <main className="pt-20 p-4 max-w-7xl mx-auto min-h-screen">
             <h1 className="text-center mb-4 opacity-100"> I am not a photographer. And I dont know why I made this.</h1>
-            <div className="columns-2 sm:columns-4 md:columns-4 lg:columns-5 gap-2">
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/blinds.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/amusement.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><video src="/metro.mp4" autoPlay muted loop></video></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/hill.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/mirror.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><video src="/scratch.mp4" autoPlay muted loop></video></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><video src="/stream.mp4" autoPlay muted loop></video></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/restaurant.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/stairs.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/codes.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/updown.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/sunset.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><video src="/laptop2.mp4" autoPlay muted loop></video></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/stand.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/cola.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/metro2.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/sunset_out.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/station.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/laptop.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/speaking.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/silly.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/cloud.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/jackboys.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/harry.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/gundu.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/road.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/catinabox.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/bsod.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/etpsunset.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/dawgs.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/pringhouse.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/umbrella.jpg" alt="test"></img></div>
-            <div className="mx-auto h-auto block w-full mb-2 rounded-lg"><img src="/terrace.jpg" alt="test"></img></div>
+            <div className="columns-2 sm:columns-4 md:columns-4 lg:columns-5 gap-4">
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/blinds.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/amusement.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><video className="rounded-2xl" src="/metro.mp4" autoPlay muted loop></video></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/hill.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/mirror.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><video className="rounded-2xl" src="/scratch.mp4" autoPlay muted loop></video></div>
+            <div className="mx-auto h-auto block w-full mb-4"><video className="rounded-2xl" src="/stream.mp4" autoPlay muted loop></video></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/restaurant.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/stairs.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/codes.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/updown.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/sunset.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><video className="rounded-2xl" src="/laptop2.mp4" autoPlay muted loop></video></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/stand.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/cola.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/metro2.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/sunset_out.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/station.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/laptop.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/speaking.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/silly.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/cloud.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/jackboys.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/harry.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/gundu.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/road.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/catinabox.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/bsod.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/etpsunset.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/dawgs.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/pringhouse.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/umbrella.jpg" alt="test"></img></div>
+            <div className="mx-auto h-auto block w-full mb-4"><img className="rounded-2xl" src="/terrace.jpg" alt="test"></img></div>
                 
             </div>
         </main>
